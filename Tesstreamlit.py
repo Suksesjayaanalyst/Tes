b509011c0aa77c880471d67c918f30c7c80650ef
Suksesjayaanalyst <fotoproducttpf@gmail.com>
2025-04-11 09:51:51 +0700

    Update Tesstreamlit.py

--- Tesstreamlit.py
+++ Tesstreamlit.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-with open("/mount/admin/install_path") as f:
-    install_path = f.read().strip()
+try:
+    with open("/mount/admin/install_path") as f:
+        install_path = f.read().strip()
+except FileNotFoundError:
+    install_path = "Not found (default or dev environment)"
 
 st.title("Hello Streamlit 👋")
+st.write(f"Install Path: {install_path}")
